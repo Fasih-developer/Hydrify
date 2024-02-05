fd33515bad42f9a560d1b992dea0c2cecce84f14
Fasih Ullah <fasih997@example.com>
2024-02-05 22:53:45 +0500

    page-3

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
+// function locomotive() {
+//     gsap.registerPlugin(ScrollTrigger);
+
+//     // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+    
+//     const locoScroll = new LocomotiveScroll({
+//       el: document.querySelector(".main"),
+//       smooth: true
+//     });
+//     // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+//     locoScroll.on("scroll", ScrollTrigger.update);
+    
+//     // tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
+//     ScrollTrigger.scrollerProxy(".main", {
+//       scrollTop(value) {
+//         return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+//       }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+//       getBoundingClientRect() {
+//         return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+//       },
+//       // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+//       pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+//     });
+//     // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+//     ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+    
+//     // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+//     ScrollTrigger.refresh();
+    
+// }
+// locomotive()
+// ----------------------------animation-----------------------
 let navbar = document.querySelector('.navbar');
 let bars = document.querySelector('nav i');
 let closex = document.querySelector('.nav-close i');
@@ -40,6 +72,7 @@ gsap.to('.page-2', {
         trigger : '.page-1',
         start:'top top',
         end:'bottom 65%',
+        // markers:true,
         pin : true,
     },
     y:-300,
@@ -47,13 +80,56 @@ gsap.to('.page-2', {
 gsap.to('.page-1', {
     scrollTrigger:{
         trigger : '.page-2',
-        start:'-170% top',
+        start:'-170% 20%',
         end:'top 60%',
         scrub:true,
     },
     opacity: 0.9,
     scale:0.9,
-    filter:' blur(6px)',
+    filter:' blur(5px)',
 
 })
 
+gsap.to('.page-3', {
+    scrollTrigger:{
+        trigger : '.page-3',
+        start:'top top',
+        end:'bottom 60%',
+        pin:'.page-3',
+        markers:true,
+        scrub:true,
+    },
+})
+gsap.to('#card4', {
+    scrollTrigger:{
+        trigger : '#card4',
+        start:'-23% top',
+        // markers:true,
+        end:'-18% top',
+        scrub:true,
+    },
+    ease:'power2.inout',
+    y:-1200,
+})
+gsap.to('#card3', {
+    scrollTrigger:{
+        trigger : '#card4',
+        start:'-5% top',
+        // markers:true,
+        end:'1% top',
+        scrub:true,
+    },
+    ease:'power2.inout',
+    y:-1200,
+})
+gsap.to('#card2', {
+    scrollTrigger:{
+        trigger : '#card4',
+        start:'15% top',
+        // markers:true,
+        end:'20% top',
+        scrub:true,
+    },
+    ease:'power2.inout',
+    y:-1200,
+})
